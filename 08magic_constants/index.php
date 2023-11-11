cf20f365fd7d2fe8d196055ae1932a849ecbae12
Claudio Zimmermann Junior <claudio.zi.junior@gmail.com>
2023-11-10 22:33:58 -0400

    deleting a variable

--- 08magic_constants/index.php
+++ 08magic_constants/index.php
@@ -10,3 +10,19 @@ const CONSTANT1 = "hii";
 
 //can't be declared from within a class, can be declared inside if statements.
 define('CONSTANT2', 'hallo');
+
+//deleting variables
+
+$names = ['John', 'Mary', 'Jane'];
+
+//it deletes the cell of the array
+unset($names[1]);
+
+//it prints the array into the screen
+print_r($names);
+echo "\n";
+
+//reindexing the array
+$names = array_values($names);
+print_r($names);
+echo "\n";
