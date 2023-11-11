0149b7482af76a3b087cae0224f5f52bb96ee599
Claudio Zimmermann Junior <claudio.zi.junior@gmail.com>
2023-11-10 22:26:29 -0400

    ways to define constants

--- 08magic_constants/index.php
+++ 08magic_constants/index.php
@@ -2,3 +2,11 @@
 
 /* Magic constats are very useful. For exemple the constant __DIR__ stores the path to the current file. */
 echo __DIR__ . "\n\n"; //that's awesome!!!
+
+//two ways to define constants
+
+//can be declared from within a class, can't be declared inside if statements.
+const CONSTANT1 = "hii";
+
+//can't be declared from within a class, can be declared inside if statements.
+define('CONSTANT2', 'hallo');
