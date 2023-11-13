83b8a9bde0252dbd741bb8878926dc68dd4f9e33
Claudio Zimmermann Junior <claudio.zi.junior@gmail.com>
2023-11-13 16:44:01 -0400

    Lesson: named args

--- 10spread_operator_named_args/index.php
+++ 10spread_operator_named_args/index.php
@@ -6,5 +6,11 @@ function sum(int|float ...$numbers)
   return array_sum($numbers);
 }
 
+function sub($a, $b)
+{
+  return $a - $b;
+}
+
 echo sum(5, 8, 15, 16, -9);
+echo "\n" . sub(b: 2, a: 6);
 echo "\n\n";
