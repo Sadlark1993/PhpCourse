9958d862a04841087fc9f34933662711bc92534e
Claudio Zimmermann Junior <Claudio.zi.junior@gmail.com>
2023-12-01 09:56:05 -0400

    Lesson: Callable functions

--- 13anonymous_and_arrow_functions/callable.php
+++ 13anonymous_and_arrow_functions/callable.php
@@ -6,12 +6,19 @@ $multiplier = 2;
 //arrow functions must have only one expression. You cant put more than one line in it.
 $multiply = fn ($num) => $num * $multiplier;
 
+function iWillMutiply($num)
+{
+  return $num * 2;
+}
+
 //the 'use' keyword provides access of a copy of a variable that are outside the funcion
-$sum = function ($a, $b, $callback) use ($multiplier) {
+//if you define the argument as callable type, you can pass a string with the name of the callable function
+$sum = function (int|float $a, int|float $b, callable $callback) use ($multiplier) {
   echo $multiplier . "\n";
   return $callback($a + $b);
 };
 
-echo $sum(5, 2, $multiply);
+echo $sum(5, 2, fn ($num) => $num * $multiplier);
 echo "\nnew multiplier: " . $multiplier;
+//echo "\nnew return: " . $sum(5, 3, "iWillMultiply"); //now working
 echo "\n\n";
--- tests_and_practices/php_api/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-const DB_HOST = '192.168.115.120';
-const DB_PORT = '5432';
-const DB_NAME = 'pbx';
-const DB_USER = 'contacte';
-const DB_PASS = 'ctepgSQL';
