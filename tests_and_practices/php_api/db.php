f857daef3eca2c766d67fa9498f14aa17874cb26
Claudio Zimmermann Junior <Claudio.zi.junior@gmail.com>
2023-12-01 08:54:35 -0400

    Created a very simple API

---
+++ tests_and_practices/php_api/db.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'config.php';
+
+class Db {
+  private $query;
+  private $connection;
+
+  public connect() {
+    $this->connection = pg_connect(sprintf('host=%s port=%s dbname=%s user=%s password=%s', DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS));
+  }
+  
+}
