bfc16c4b47f14a4ce2f8046a41dce0e33c95ceb5
Claudio Zimmermann Junior <Claudio.zi.junior@gmail.com>
2023-12-28 10:55:45 -0400

    OOP Lesson: Nul-safe operator

--- 02OOP/01magic_methods/index.php
+++ 02OOP/01magic_methods/index.php
@@ -8,7 +8,11 @@ require_once 'PropertyPromotion.php';
 $myAccount = new Account('Junior', 9.5);
 $johnsAccount = new PropertyPromotion('John', 4.8);
 
+//method chaining
 $myAccount->deposit(50)->deposit(30);
+
+//this will not execute because fo the null-safe operator '?'.
+$silvasAccount?->deposit(20);
 
 echo $myAccount->balance . "\n";
 echo $johnsAccount->balance . "\n";
