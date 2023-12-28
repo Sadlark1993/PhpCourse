187a13f8a6d0615f3bcab285bde58f008b9dc978
Claudio Zimmermann Junior <Claudio.zi.junior@gmail.com>
2023-12-28 11:14:49 -0400

    Lesson: namespaces

---
+++ 02OOP/01classes_and_methods/index.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+require_once 'Account.php';
+require_once 'PropertyPromotion.php';
+
+$myAccount = new Account('Junior', 9.5);
+$johnsAccount = new PropertyPromotion('John', 4.8);
+
+//method chaining
+$myAccount->deposit(50)->deposit(30);
+
+//this will not execute because fo the null-safe operator '?'. Only php > 8.0
+$silvasAccount?->deposit(20);
+
+echo $myAccount->balance . "\n";
+echo $johnsAccount->balance . "\n";
--- 02OOP/02namespaces/index.php
+++ 02OOP/02namespaces/index.php
@@ -5,13 +5,16 @@ declare(strict_types=1);
 require_once 'Account.php';
 require_once 'PropertyPromotion.php';
 
+//importing Account class from the namespace directory App. (remember that it's backslash)
+use App\Account;
+
 $myAccount = new Account('Junior', 9.5);
 $johnsAccount = new PropertyPromotion('John', 4.8);
 
 //method chaining
 $myAccount->deposit(50)->deposit(30);
 
-//this will not execute because fo the null-safe operator '?'.
+//this will not execute because fo the null-safe operator '?'. Only php > 8.0
 $silvasAccount?->deposit(20);
 
 echo $myAccount->balance . "\n";
